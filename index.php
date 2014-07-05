6033987627ab9e0a57e7373319ebb0878752c58d
Shmo Chuang <elisajchuang@gmail.com>
2014-07-05 18:38:35 -0400

    first commit for p2

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Password Generator</title>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<?php require 'logic.php' ?>
+</head>
+<body>
+
+	<h1>xkcd Password Generator</h1>
+	
+	<!-- create a html form -->
+	<form method='POST' action='index.php'>
+		
+		<!-- ask number of words $num_words-->
+		Number of words: <input type="number" name="quantity" min="1" max="10"> (1 to 10)<br>
+		
+		<!-- ask if number desired $numeral-->
+		<input type="checkbox" name="numerals" id="numeral">Include numbers <br>
+		
+		<!-- ask if symbol desired $symbol and quantity-->
+		<input type="checkbox" name="symbols" id="symbol">Include symbols <br>
+		
+		<!-- ask if capitals desired $caps--> 
+		<input type="checkbox" name="r_caps" id="r_caps">Include random capitals<br>
+		
+		<!-- ask camel case or hyphens $camel_case, $hypens (radio buttons)-->
+		<input type="radio" name="seperator" value="camel">Camel Case or <input type="radio" name="seperator" value="hyphens">Hyphens <br>
+		
+		<!-- ask if all upper or all lower case $letter_type-->
+		<input type="radio" name="case" value="capital">ALL CAPS or <input type="radio" name="case" value="lower">all lower <br>
+
+		<!-- submit button -->
+		<input type='submit' class='btn btn-default' value='Get New Password'>
+
+	</form>
+
+</body>
+</html>
