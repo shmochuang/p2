54c4923d9ed9b2c9a9f12d23fa1a6108105c71e1
Shmo Chuang <elisajchuang@gmail.com>
2014-07-06 01:04:12 -0400

    testing rand() and for loop)

--- logic.php
+++ logic.php
@@ -1,10 +1,10 @@
 <?php
 
-// get word list - http://www.mieliestronk.com/corncob_lowercase.txt
-// parse through list into array using file()
+	// get word list - http://www.mieliestronk.com/corncob_lowercase.txt
+	// parse through list into array using file()
 	$words = file('words.txt', FILE_IGNORE_NEW_LINES| FINE_SKIP_EMPTY_LINES);
 		
-// get or post $num_words, $numeral, $symbol, $r_caps, $seperator, $case
+	// get or post $num_words, $numeral, $symbol, $r_caps, $seperator, $case
 	$num_words = $_POST["num_words"];
 	$numeral = $_POST["numerals"];
 	$symbol = $_POST["symbols"];
@@ -12,26 +12,31 @@
 	$seperator = $_POST["seperator"];
 	$case = $_POST["case"];
 	
-	echo $numeral;
-	echo $symbol;
-	echo $r_caps;
-	echo $seperator;
-	echo $case;
-
-
-// if $num_words not greater than 0 and less than 10, echo "please put number btwn 1-10"
-// this already happens in browser with input type number.
-
-// create new array for password array password
-
-// loop through $num_words amount of times
-	// get random number between 0 and number of words in list $rand_num
-	// if $word_index == rand_num, choose again
-	// else set $word_index = rand_num
-	// set password at index $i to words at index $word_index
+		// echo $numeral;  on/off
+		// echo $symbol;  on/off
+		// echo $r_caps;  on/off
+		// echo $seperator;  camel/hyphens
+		// echo $case;  capital/lower
 	
-	// if camel_case == true ucfirst
-	// if hypens == true concat hypen
-	// if all upper == true strtoupper
+	
+	// if $num_words not greater than 0 and less than 10, echo "please put number btwn 1-10"
+	// this already happens in browser with input type number.
+	
+	// create new array for password array password
+	$password = array();
 
-// echo password to index file
+	// loop through $num_words amount of times
+	for ($i = 0; i < $num_words; i++){
+		// get random number between 0 and number of words in list 
+		$word_index = rand(0, 58000);
+		echo $word_index;
+		
+		// set password at index $i to words at index $word_index
+		
+		// if camel_case == true ucfirst
+		// if hypens == true concat hypen
+		// if all upper == true strtoupper
+	}
+
+	
+	// echo password to index file
